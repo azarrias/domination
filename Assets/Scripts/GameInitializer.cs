2e685bb0ecdc0c2e88d9230d353888f70b6332ca
azarrias <adolfo.zarrias@gmail.com>
2021-05-30 16:16:31 +0200

    Upgrade project to 2020.3.4f1, add edge colliders on screen bounds

---
+++ Assets/Scripts/GameInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Initializes the game
+/// </summary>
+public class GameInitializer : MonoBehaviour 
+{
+	void Awake()
+    {
+        ScreenUtils.Initialize();
+    }
+}
---
+++ Assets/Scripts/ScreenEdgeCollider.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenEdgeCollider : MonoBehaviour
+{
+    void Awake()
+    {
+        var camera = Camera.main;
+
+        if (camera == null)
+        {
+            Debug.LogError("Main camera not found");
+        }
+
+        var bottomLeft = new Vector2(ScreenUtils.ScreenLeft, ScreenUtils.ScreenBottom);
+        var topLeft = new Vector2(ScreenUtils.ScreenLeft, ScreenUtils.ScreenTop);
+        var topRight = new Vector2(ScreenUtils.ScreenRight, ScreenUtils.ScreenTop);
+        var bottomRight = new Vector2(ScreenUtils.ScreenRight, ScreenUtils.ScreenBottom);
+
+        var edgeCollider = gameObject.AddComponent<EdgeCollider2D>();
+        edgeCollider.points = new[] { bottomLeft, topLeft, topRight, bottomRight, bottomLeft };
+    }
+}
